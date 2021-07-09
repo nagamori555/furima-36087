e854834cc8b5de94c22a03899692d5554dd702cf
nagamori555 <mobydick635@gmail.com>
2021-07-09 17:34:30 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,16 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :delivery_fee
+  belongs_to :days_to_ship
+  belongs_to :prefecture
+  belongs_to :status
+
   belongs_to :user
   has_one_attached :image
+
+  validates :category_id, numericality: {other_than: 0 , message: "can't be blank"}
+  validates :status_id, numericality: {other_than: 0 , message: "can't be blank"}
+  validates :delivery_fee_id, numericality: {other_than: 0 , message: "can't be blank"}
+  validates :prefecture_id, numericality: {other_than: 0 , message: "can't be blank"}
 end
--- db/migrate/20210709060613_create_items.rb
+++ db/migrate/20210709060613_create_items.rb
@@ -9,7 +9,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.integer :prefecture_id,           null: false
       t.integer :days_to_ship_id,         null: false
       t.integer :price,                   null: false
-      t.references :user,                 null: false
+      t.references :user,                 null: false, foreign_key: true
       t.timestamps
     end
   end
