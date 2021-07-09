be56e15b9ec00e20a1e1fa4bfd8d55a954021bad
nagamori555 <mobydick635@gmail.com>
2021-07-09 17:58:27 +0900

    バリデーション編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,8 +9,13 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :category_id, numericality: {other_than: 0 , message: "can't be blank"}
-  validates :status_id, numericality: {other_than: 0 , message: "can't be blank"}
-  validates :delivery_fee_id, numericality: {other_than: 0 , message: "can't be blank"}
-  validates :prefecture_id, numericality: {other_than: 0 , message: "can't be blank"}
+  with_options presence: true do
+    validates :item_name, length: {maximum: 40}
+    validates :description, length: {maximum: 1000}
+    validates :category_id, numericality: {other_than: 0 , message: "can't be blank"}
+    validates :status_id, numericality: {other_than: 0 , message: "can't be blank"}
+    validates :delivery_fee_id, numericality: {other_than: 0 , message: "can't be blank"}
+    validates :prefecture_id, numericality: {other_than: 0 , message: "can't be blank"}
+    validates :price, numericality: {only_integer: true, in: 300..9999999, message: "is invalid."}
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
   
   validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "is invalid. Input the alphanumeric password."}
   with_options presence: true do
-    validates :nickname, presence: true
-    validates :birth_day, presence: true
+    validates :nickname
+    validates :birth_day
     validates :last_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
     validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
     validates :last_name_kana, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
