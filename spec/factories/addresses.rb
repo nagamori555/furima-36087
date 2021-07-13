f7aa5bb412b717949b2ffdcbe972dd8a77a7137a
nagamori555 <mobydick635@gmail.com>
2021-07-13 17:10:15 +0900

    コード削除

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -1,11 +1,4 @@
 FactoryBot.define do
   factory :address do
-    postal_code                {123-456}
-    prefecture_id              {32}
-    city                       {'東京都東京区'}
-    house_number               {'江戸村明治町３ー３'}
-    building_name              {'レジデンシャルテスト'}
-    telephone                  {'09012345678'}
-    association :purchase_record
   end
 end
--- spec/models/purchase_record_spec.rb
+++ spec/models/purchase_record_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe PurchaseRecord, type: :model do
-  
 end
